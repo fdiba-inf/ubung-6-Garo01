8612a2e9f9140fbe38c7bf4849e40822c53b3e3e
Garo Garo <garoaprahamyan23@gmail.com>
2020-11-10 11:44:21 +0000

    pali

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,20 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        assert number > 0;
+        int palnumber = number;
+        int reverse = 0;
+        while (number>0){
+          reverse = reverse*10 + number%10;
+          number= number /10;
+        }
+        System.out.println (reverse);
+        if (palnumber==reverse){
+          System.out.println ("Palindrome");
+        }else{ 
+          System.out.println ("Palindrome iok");
+        }
+        }
     }
-}
+
